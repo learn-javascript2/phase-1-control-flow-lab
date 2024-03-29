3ae571066318553cdbc898d64860fee103f69a02
andyjohn23 <andrewnick906@gmail.com>
2024-03-29 11:47:45 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return "This one is on me!"
+  } else if (feet > 400 && feet < 2000) {
+    return "That will be twenty bucks."
+  } else if (feet > 2500) {
+    return "No can do."
+  } else {
+    return "I will gladly take your thirty bucks."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  // city === "NYC" ? ("Ok, sounds good.") : ("No go.");
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much."
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+      break;
+  }
 }
